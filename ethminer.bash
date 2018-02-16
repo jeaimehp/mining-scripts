22d304b94952e76179face2e548541d3373aeb2c
Jeaime Powell <jeaimehp@gmail.com>
2018-02-15 22:40:29 -0600

    dir upload

---
+++ ethminer.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+#set GPU_FORCE_64BIT_PTR=0
+#set GPU_MAX_HEAP_SIZE=100
+#set GPU_USE_SYNC_OBJECTS=1
+#set GPU_MAX_ALLOC_PERCENT=100
+#set GPU_SINGLE_ALLOC_PERCENT=100
+/home/jeaimehp/ethminer/build/ethminer/ethminer -G -S eth.pool.minergate.com:45791 -O jeaimehp@gmail.com 
---
+++ keep_trucking.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+while [ 1 ]; do $1 && break; done
